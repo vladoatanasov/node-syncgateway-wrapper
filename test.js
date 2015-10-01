d121d599a51b1b3c28e992384f3b5690f4b49aac
Vlado Atanasov <vlado.atanasov@gmail.com>
2015-10-01 22:50:24 +0100

    * implemented /DB * implemented /DB/_all_docs

--- test.js
+++ test.js
@@ -1,10 +1,15 @@
-var SyncGateway = require('./index.js');
-
-var wrapper = new SyncGateway({
-    host: "http://localhost:4985",
-    bucket: "default"
-});
+var SyncGateway = require('./index.js'),
+    wrapper = new SyncGateway({
+        host: "http://localhost:4985",
+        bucket: "default"
+    }),
+    assert = require('assert')
 
 wrapper.getDb(function (response) {
+    assert.equal(response.db_name, 'default', 'assert db name failed');
+    console.log(response);
+});
+
+wrapper.getAllDocs(function (response) {
     console.log(response);
 });
